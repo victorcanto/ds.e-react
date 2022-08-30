3f2e64eae8f595710978d023fd513abc6d583e40
Victor Canto <iamvictorcanto@gmail.com>
2022-08-29 23:40:04 -0300

    test: imported and used Color component

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Button } from '@ds.e/react';
 
-import '@ds.e/scss/lib/Button.css';
+import { Color } from '@ds.e/react';
+import '@ds.e/scss/lib/Utilities.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(<Button type="button" label='Example button' />);
+root.render(<Color hexCode='blue' width='lg' height='lg' />);
