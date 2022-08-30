cc7c4313f45abae7c8f3846f62cd572dbd1b1595
Victor Canto <iamvictorcanto@gmail.com>
2022-08-29 22:40:53 -0300

    feat: using class from Button atom

--- packages/react/src/atoms/Button.tsx
+++ packages/react/src/atoms/Button.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
 }
 
-const Button = ({ label }: ButtonProps) => {
-  return <button>{label}</button>;
+const Button = ({ label, ...restProps }: ButtonProps) => {
+  return (
+    <button {...restProps} className='dse-button__container'>
+      {label}
+    </button>
+  );
 };
 
 export default Button;
