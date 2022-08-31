aeb996807739958b4703e22aa931234c1061a744
Victor Canto <iamvictorcanto@gmail.com>
2022-08-31 14:26:35 -0300

    fix: changes extension type to js

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   testRegex: '(/.*\\.test)\\.(ts|tsx)$',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
 };
